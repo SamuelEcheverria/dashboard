be92e0ac867403ca2e5a376f7429ef51389a05ab
SamuelEcheverria <sechever@espol.edu.ec>
2026-07-08 18:59:39 +0000

    feat(selector): add useState and bind value in SelectorUI

--- dashboard/src/components/SelectorUI.tsx
+++ dashboard/src/components/SelectorUI.tsx
@@ -1,28 +1,32 @@
+import { useState } from 'react';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select, { type SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
 export default function SelectorUI() {
-    const handleChange = (event: SelectChangeEvent<string>) => {
-        alert(event.target.value);
-    };
+    const [cityInput, setCityInput] = useState('');
 
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setCityInput(event.target.value);
+    };
+                <Select
     return (
         <FormControl fullWidth>
       <InputLabel id="city-select-label">Ciudad</InputLabel>
-      <Select
-         labelId="city-select-label"
+                    value={cityInput}
+                    onChange={handleChange}>
+                    <MenuItem disabled value=""><em>Seleccione una ciudad</em></MenuItem>
          id="city-simple-select"
          label="Ciudad"
          onChange={handleChange}>
          <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
          <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
          <MenuItem value={"quito"}>Quito</MenuItem>
-         <MenuItem value={"manta"}>Manta</MenuItem>
-         <MenuItem value={"cuenca"}>Cuenca</MenuItem>
-      </Select>
 
-    </FormControl>
-    )
+            {cityInput && (
+                <p>
+                    Información del clima en <span style={{ textTransform: 'capitalize', fontWeight: 'bold' }}>{cityInput}</span>
+                </p>
+            )}
 }
