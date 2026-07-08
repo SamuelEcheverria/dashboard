5d4f9f32552c2f6af309fc582eaafc2fbeb04e34
SamuelEcheverria <sechever@espol.edu.ec>
2026-07-08 18:45:30 +0000

    feat(selector): add state and onChange handler to SelectorUI; use Selector in App

---
+++ dashboard/src/components/SelectorUI.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select, { type SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function SelectorUI() {
+   const [cityInput, setCityInput] = useState('');
+
+   const handleChange = (event: SelectChangeEvent<string>) => {
+      setCityInput(event.target.value);
+      alert(event.target.value);
+   };
+
+   return (
+      <div>
+         <FormControl fullWidth>
+            <InputLabel id="city-select-label">Ciudad</InputLabel>
+            <Select
+               labelId="city-select-label"
+               id="city-simple-select"
+               label="Ciudad"
+               value={cityInput}
+               onChange={handleChange}>
+               <MenuItem disabled value=""><em>Seleccione una ciudad</em></MenuItem>
+               <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
+               <MenuItem value={"quito"}>Quito</MenuItem>
+               <MenuItem value={"manta"}>Manta</MenuItem>
+               <MenuItem value={"cuenca"}>Cuenca</MenuItem>
+            </Select>
+         </FormControl>
+
+         {cityInput && (
+            <p>
+               Información del clima en <span style={{ textTransform: 'capitalize', fontWeight: 'bold' }}>{cityInput}</span>
+            </p>
+         )}
+      </div>
+   );
+}
